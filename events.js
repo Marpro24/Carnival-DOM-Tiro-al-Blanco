8fffdcbead079c07112faf0fde99c7d3a87226e6
Paola Andrea Morales Petro <paola@paolas-macbook-pro.home>
2024-12-12 20:16:09 +0100

    start game

--- events.js
+++ events.js
@@ -6,20 +6,37 @@
 //     scope.style.top = e.pageY-150+ "px";
 //   });
 
-var timeLeft = 30;
-var elem = document.getElementById('some_div');
-var timerId = setInterval(countdown, 1000);
+// var timeLeft = 30;
+// var elem = document.getElementById('some_div');
+// var timerId = setInterval(countdown, 1000);
 
-function countdown() {
-    if (timeLeft == -1) {
-        clearTimeout(timerId);
-        doSomething();
-    } else {
-        elem.innerHTML = timeLeft + ' seconds remaining';
-        timeLeft--;
-    }
+// function countdown() {
+//     if (timeLeft == -1) {
+//         clearTimeout(timerId);
+//         doSomething();
+//     } else {
+//         elem.innerHTML = timeLeft + ' seconds remaining';
+//         timeLeft--;
+//     }
+// }
+
+// function doSomething() {
+//     alert("Hi");
+// }
+
+function startGame() {
+    // Coge el valor del elmento input de nombre del form
+    const playerName = document.getElementById("id_name").value;
+
+    // Pone el nombre en el elemento de player name bienvenida
+    document.getElementById("player_name").innerHTML = playerName;
+
+    // Esconde player form
+    document.getElementById("player_form").setAttribute("hidden", "");
+
+    // Muestra en lugar el elemento de game play
+    document.getElementById("game_play").removeAttribute("hidden");
 }
 
-function doSomething() {
-    alert("Hi");
-}
+document.getElementById("player_form_submit_button").addEventListener("click", startGame);
+
