9f48a6100277d6626735336a4470220f23071b44
Paola Andrea Morales Petro <paola@paolas-macbook-pro.home>
2024-12-13 12:01:52 +0100

    actualizacion codigo1

--- events.js
+++ events.js
@@ -1,28 +1,4 @@
-// let scope = document.querySelector(".scope");
-
-//  //scope movement
-//  document.scope.addEventListener("mousemove", (e) => {
-//     scope.style.left = e.pageX-150+ "px";
-//     scope.style.top = e.pageY-150+ "px";
-//   });
-
-// var timeLeft = 30;
-// var elem = document.getElementById('some_div');
-// var timerId = setInterval(countdown, 1000);
-
-// function countdown() {
-//     if (timeLeft == -1) {
-//         clearTimeout(timerId);
-//         doSomething();
-//     } else {
-//         elem.innerHTML = timeLeft + ' seconds remaining';
-//         timeLeft--;
-//     }
-// }
-
-// function doSomething() {
-//     alert("Hi");
-// }
+//  Start Game 
 
 function startGame() {
     // Coge el valor del elmento input de nombre del form
@@ -36,7 +12,18 @@ function startGame() {
 
     // Muestra en lugar el elemento de game play
     document.getElementById("game_play").removeAttribute("hidden");
+
+    // Mostrar botones 
+    document.getElementById("start_button").removeAttribute("hidden");
+    document.getElementById("reset_button").removeAttribute("hidden");
 }
 
 document.getElementById("player_form_submit_button").addEventListener("click", startGame);
+
+// Botones start y reset
+
+document.getElementById("start_button").setAttribute("hidden", "")
+document.getElementById("reset_button").setAttribute("hidden","")
+
+
 
